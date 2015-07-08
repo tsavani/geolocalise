dbc3b380e11fdac10f87d0a58f04192e38489b51
twinks14 <svnsavani0@gmail.com>
2015-07-08 18:19:30 +0530

    Remove unwanted comments + codes :relaxed: :relaxed:

--- lib/geolocalise.rb
+++ lib/geolocalise.rb
@@ -2,28 +2,23 @@ require "geolocalise/version"
 require 'geocoder'
 
 module Geolocalise
-  # Your code goes here...
   def self.get_city(location)
-     #location_is_string(location) ? Geocoder.search(location).first.city : 'Please Enter proper value'
-     if (location =~ /[[:digit:]]/) == nil
-    	Geocoder.search(location).first.city
-     else
+    if (location =~ /[[:digit:]]/) == nil
+      Geocoder.search(location).first.city
+    else
     	check_geo_cords(location) ? Geocoder.search(location).first.city : nil
   	end
-     #check_geo_cords(location) ? Geocoder.search(location).first.city : "Please Enter proper co-ordinates"
   end
 
   def self.get_country(location)
-    #location_is_string(location) ? Geocoder.search(location).first.country : 'Please Enter proper value'
     if (location =~ /[[:digit:]]/) == nil
-    	Geocoder.search(location).first.country
+      Geocoder.search(location).first.country
     else
     	check_geo_cords(location) ? Geocoder.search(location).first.country : nil
   	end
   end
 
   def self.get_state(location)
-    #location_is_string(location) ? Geocoder.search(location).first.state : 'Please Enter proper value'
     if (location =~ /[[:digit:]]/) == nil
     	Geocoder.search(location).first.state
     else
@@ -32,7 +27,6 @@ module Geolocalise
   end
 
   def self.get_country_code(location)
-  	#location_is_string(location) ? Geocoder.search(location).first.country_code : 'Please Enter proper value'
     if (location =~ /[[:digit:]]/) == nil
     	Geocoder.search(location).first.country_code
     else
@@ -41,33 +35,25 @@ module Geolocalise
   end
 
   def self.get_postal_code(location)
-    #check_geo_cords(location) ? Geocoder.search(location).first.postal_code : "Please Enter proper co-ordinates"
-    #cords = Geocoder.coordinates(location)
     if (location =~ /[[:digit:]]/) == nil
     	Geocoder.search(Geocoder.coordinates(location).map(&:inspect).join(',')).first.postal_code
     else
-    #Geocoder.search(Geocoder.coordinates(location).map(&:inspect).join(',')).first.postal_code
-    check_geo_cords(location) ? Geocoder.search(location).first.postal_code : nil
+      check_geo_cords(location) ? Geocoder.search(location).first.postal_code : nil
     end
   end
 
   private
 
-    def self.check_geo_cords(location)
-    	#location.is_a?(String) ? location : location = location.map(&:inspect).join(',')
-      latitude, longitude = location.split(',')[0], location.split(',')[1]
-      if latitude.to_i >= -90 and latitude.to_i <= 90 and (latitude =~ /[[:alpha:]]/) == nil
-         if longitude.to_i >= -180 and longitude.to_i <= 180 and (longitude =~ /[[:alpha:]]/) == nil
-           true
-         else
-           false
-         end
-      else
-        false
-      end
+  def self.check_geo_cords(location)
+    latitude, longitude = location.split(',')[0], location.split(',')[1]
+    if latitude.to_i >= -90 and latitude.to_i <= 90 and (latitude =~ /[[:alpha:]]/) == nil
+       if longitude.to_i >= -180 and longitude.to_i <= 180 and (longitude =~ /[[:alpha:]]/) == nil
+         true
+       else
+         false
+       end
+    else
+      false
     end
-
-  # 	def self.location_is_string(location)
-  # 		 (location =~ /[[:digit:]]/) == nil
-  # 	end
+  end
 end
--- spec/geolocalise_spec.rb
+++ spec/geolocalise_spec.rb
@@ -11,59 +11,58 @@ describe Geolocalise do
   end
 
   it "should return error" do 
-  	Geolocalise.get_city("19.075as, Mumbai").should == nil
+    Geolocalise.get_city("19.075as, Mumbai").should == nil
   end
 
    # Testing geolocalise and should return exact state name when input in [geocords or city,state name]
-   it "should return state" do
+  it "should return state" do
     Geolocalise.get_state("Mumbai").should == "Maharashtra"
   end
 
-   it "should return state error" do 
-  	Geolocalise.get_state("Mumbai, 72.8776559").should == nil
+  it "should return state error" do 
+    Geolocalise.get_state("Mumbai, 72.8776559").should == nil
   end
 
-   it "should return state by coords" do 
-  	Geolocalise.get_state("19.07598, 72.8776559").should == "Maharashtra"
-   end
+  it "should return state by coords" do 
+    Geolocalise.get_state("19.07598, 72.8776559").should == "Maharashtra"
+  end
 
     # Testing geolocalise and should return exact country name when input in [geocords or city,state name]
-   it "should return country" do
+  it "should return country" do
     Geolocalise.get_country("19.0759837, 72.8776559").should == "India"
-   end
+  end
 
-   it "should return country error" do 
-  	Geolocalise.get_state("Gujarat, 72.8776559").should == nil
-   end
+  it "should return country error" do 
+    Geolocalise.get_state("Gujarat, 72.8776559").should == nil
+  end
 
-   it "should return country by coords" do 
-  	Geolocalise.get_state("19.07598, 72.8776559").should == "Maharashtra"
-   end
+  it "should return country by coords" do 
+    Geolocalise.get_state("19.07598, 72.8776559").should == "Maharashtra"
+  end
 
     # Testing geolocalise and should return exact Country Code when input as [geocords or city,state and country name]
-   it "should return get country code" do
+  it "should return get country code" do
     Geolocalise.get_country_code("19.0759837, 72.8776559").should == "IN"
   end
 
-   it "should return country error" do 
-  	Geolocalise.get_country_code("Gujarat, 72.8776559").should == nil
-   end
+  it "should return country error" do 
+    Geolocalise.get_country_code("Gujarat, 72.8776559").should == nil
+  end
 
-   it "should return country by coords" do 
-  	Geolocalise.get_country_code("19.07598, 72.8776559").should == "IN"
-   end
+  it "should return country by coords" do 
+    Geolocalise.get_country_code("19.07598, 72.8776559").should == "IN"
+  end
 
    # Testing geolocalise and should return exact Country Code when input as [geocords or city,state and country name]
-   it "should return get postal code" do
+  it "should return get postal code" do
     Geolocalise.get_postal_code("Surat").should == "395007"
-   end
+  end
   
-   it "should return postal cords by coords" do 
-  	Geolocalise.get_postal_code("19.07598, 72.8776559").should == "400070"
-   end
+  it "should return postal cords by coords" do 
+    Geolocalise.get_postal_code("19.07598, 72.8776559").should == "400070"
+  end
 
-   it "should return erorr when cords is not proper" do 
-  	Geolocalise.get_postal_code("19.07598, mumbai").should == nil
-   end
-   
+  it "should return erorr when cords is not proper" do 
+    Geolocalise.get_postal_code("19.07598, mumbai").should == nil
+  end
 end
